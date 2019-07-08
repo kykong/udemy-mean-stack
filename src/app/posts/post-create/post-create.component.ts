c55814dffab6802591f1aad57cbb1b4605d47520
kykong <ky_kong@yahoo.com>
2019-07-08 21:05:26 +0800

    Added validation to ensure title and content are not empty.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { Post } from '../post.model';
+import { format } from 'path';
 
 @Component({
   selector:    'app-post-create',
@@ -14,6 +15,10 @@ export class PostCreateComponent {
   @Output() postCreated = new EventEmitter<Post>();
 
   onAddPost(postForm:NgForm) {
+    if (postForm.invalid) {
+      return;
+    }
+
     const post: Post = {
       title: postForm.value.title,
       content: postForm.value.content
