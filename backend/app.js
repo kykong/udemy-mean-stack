95711cc8d8c96159387f5ec1b838fdc420449ece
Kong Kim Yuan <kykong@Agamemnon.local>
2019-07-12 00:05:24 +0800

    Initial RESTful implementation

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+const posts = [
+     {id: "sdkufylkj3r", title: 'First post', content: 'This is the first post'},
+     {id: "sdku;lk;oe", title: 'Second post', content: 'This is the second post'},
+     {id: "sdkuf.,m[popdf", title: 'Third post', content: 'This is the third post'}
+];
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use("/api/posts", (req, res, next) => {
+  res.status(200)
+     .json({
+       message: "Posts fetched successfully",
+       posts
+      });
 });
 
 module.exports = app;
